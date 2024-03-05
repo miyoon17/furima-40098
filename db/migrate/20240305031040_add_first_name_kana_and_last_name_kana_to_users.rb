2dd8f43d599e34e7b92a72792758d4e7d62c8ea2
miyoon17 <153158257+miyoon17@users.noreply.github.com>
2024-03-05 16:14:05 +0900

    マイグレーションファイル微調整

--- db/migrate/20240305031040_add_first_name_kana_and_last_name_kana_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddFirstNameKanaAndLastNameKanaToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :first_name_kana, :string
-    add_column :users, :last_name_kana, :string
-  end
-end
--- db/migrate/20240305031454_add_birth_date_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddBirthDateToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :birthday, :date
-  end
-end
