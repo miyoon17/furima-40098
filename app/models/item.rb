50f0315fa81b05f7532ac93a15a33c0c7bc48edc
miyoon17 <153158257+miyoon17@users.noreply.github.com>
2024-03-05 15:39:48 +0900

    コメントアウト修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,14 @@
-=begin
 class Item < ApplicationRecord
-  extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :cost
-  belongs_to :prefecture
-  belongs_to :scheduled_day
+  # extend ActiveHash::Associations::ActiveRecordExtensions
+  # belongs_to :category
+  # belongs_to :condition
+  # belongs_to :cost
+  # belongs_to :prefecture
+  # belongs_to :scheduled_day
 
-  #空の投稿を保存できないようにする
-  validates :title, :text, presence: true
+  # #空の投稿を保存できないようにする
+  # validates :title, :text, presence: true
 
-  #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, condition_id, cost_id, prefecture_id, scheduled_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  # #ジャンルの選択が「---」の時は保存できないようにする
+  # validates :category_id, condition_id, cost_id, prefecture_id, scheduled_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
-=end
