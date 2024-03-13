db07ed4a4caa1ae5fffb7802a356f7ed0cf571dc
miyoon17 <153158257+miyoon17@users.noreply.github.com>
2024-03-13 17:20:23 +0900

    商品出品機能 0313 02

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,26 @@
-
 class Item < ApplicationRecord
-  # extend ActiveHash::Associations::ActiveRecordExtensions
-  # belongs_to :category
-  # belongs_to :condition
-  # belongs_to :cost
-  # belongs_to :prefecture
-  # belongs_to :scheduled_day
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :cost
+  belongs_to :prefecture
+  belongs_to :scheduled_day
+  belongs_to :user
+  has_one_attached :image
 
-  # #空の投稿を保存できないようにする
-  # validates :title, :text, presence: true
+  #空の投稿を保存できないようにする
 
-  # #ジャンルの選択が「---」の時は保存できないようにする
-  # validates :category_id, condition_id, cost_id, prefecture_id, scheduled_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  with_options presence: true do
+
+  validates :image
+  validates :name
+  validates :description
+  validates :category_id, :condition_id, :cost_id, :prefecture_id, :scheduled_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank"}
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+end
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  with_options numericality: { other_than: 0 } do
+  validates :category_id, :condition_id, :cost_id, :prefecture_id, :scheduled_day_id, presence: true
+ end
 end
